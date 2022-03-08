1991940eb78d19829da6612815d3d95de158439f
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-08 10:51:24 +0100

    dev: added components

---
+++ front/PhotoBook/src/routes/Legal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Legal = () => {
+  return (
+    <View>
+      <Text>Legal works!</Text>
+    </View>
+  );
+};
+
+export default Legal;
