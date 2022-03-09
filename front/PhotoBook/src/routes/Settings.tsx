bc16ad51e6dd14121c1eb8b7b580992a3c0cb390
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-09 12:11:58 +0100

    dev: added disconnect

--- front/PhotoBook/src/routes/Settings.tsx
+++ front/PhotoBook/src/routes/Settings.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Button} from 'react-native-elements';
+import {api} from '../api';
+import {useAppDispatch} from '../redux/hooks';
+import {disconnect} from '../redux/slices/authentication.slice';
 
 const Settings = () => {
+  const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = async () => {
+    try {
+      setIsLoading(true);
+      await api.disconnect();
+      setIsLoading(false);
+      dispatch(disconnect(undefined));
+    } catch (err) {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>Settings works!</Text>
+    <View style={styles.container}>
+      <Button title={'Disconnect'} onPress={onSubmit} loading={isLoading} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 10,
+  },
+});
 
 export default Settings;
